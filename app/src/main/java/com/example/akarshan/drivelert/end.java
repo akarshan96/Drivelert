47a5e08188d140f3875a8c7e12e2eaaa501811d2
Akarshan Gandotra <akarshan.gandotra@gmail.com>
2025-08-31 02:09:56 +0530

    Migrate project to AndroidX and update SDK

--- app/src/main/java/com/example/akarshan/drivelert/end.java
+++ app/src/main/java/com/example/akarshan/drivelert/end.java
@@ -1,7 +1,7 @@
 package com.example.akarshan.drivelert;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
